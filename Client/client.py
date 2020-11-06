16941607f6cc9f7fbc48fe51e1b12e2526a17e0c
Dani <flamegiraffes@gmail.com>
2020-11-06 12:50:23 -0500

    Initial commit
    Info from IP

---
+++ Client/client.py
@@ -0,0 +1,46 @@
+# pip install requests
+import requests
+# pip install scapy
+# from scapy.all import *
+# from scapy.arch.windows import IFACES
+from math import sin, cos, sqrt, atan2, radians
+
+
+def main():
+    getBasicIPInfo()
+
+
+def calculateDistance(lat1, lon1, lat2, lon2):
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c
+
+
+def getBasicIPInfo():
+    # GET MY IP
+    myIP = requests.get('https://api.ipify.org').text
+    print(f"My public IP address is: {myIP}")
+
+    # GET INFO BASED ON IP
+    ipInfo = requests.get(url=f"http://ip-api.com/json/{myIP}").json()
+    # print(ipInfo)
+    print(f"Your ISP: {ipInfo['isp']} \n"
+          f"Your location: {ipInfo['lat']}, {ipInfo['lon']}")
+    distanceToServer = calculateDistance(ipInfo['lat'], ipInfo['lon'], 47.414259, 8.549612)
+
+    # UZH coordinates: (47.414259, 8.549612)
+    print(f"Distance to server is {distanceToServer} kilometers.")
+
+
+main()
