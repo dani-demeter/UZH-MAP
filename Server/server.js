cd824c7c4e18d2ff38fda66c37e6403d641617ea
Lawand97 <lawand.97@hotmail.com>
2020-11-23 15:38:30 +0100

    packet sniff on server

--- Server/server.js
+++ Server/server.js
@@ -5,10 +5,26 @@ const express = require('express')
 var path = require('path');
 const app = express()
 const port = process.env.PORT || 3000 // for heroku
+const axios = require('axios');
+
+
 
 app.get('/HTML', (req, res) => {
   console.log("Sending HTML");
+  var ip = req.connection.remoteAddress
+  var str1 = "http://127.0.0.1:3005/"; //executes the python script
+  var str2 = "ip";
+  var micros = str1.concat(str2);
+  axios.get(micros)
+  .then(response => {
+    console.log(response);
+    console.log(response.data.explanation);
+  })
+  .catch(error => {
+    console.log(error);
+  });
   res.sendFile(path.join(__dirname + '/index.html'));
+  //send scapy file res.sendFile(path.join(__dirname + '/index.html'));
 })
 app.get('/video', (req, res) => {
     console.log("Sending video");
--- Server/server.py
+++ Server/server.py
@@ -1,9 +1,35 @@
-import http.server
+from http.server import *
 import socketserver
+import pickle
+from scapy.all import *
+PORT = 3005
+#Handler = http.server.SimpleHTTPRequestHandler
 
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+def pi(filename,packets):
+    outfile = open(filename,'wb')
+    pickle.dump(packets,outfile)
+    outfile.close()
+    
+class myHandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        packets =  sniff(timeout=5)
+        filename = 'sniff'
+        print(self.path)
+        filename = filename + self.path[1:]
+        pi(filename,packets)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        #self.send_header("Content-length", len(b))
+        self.end_headers()
+        self.wfile.write('HELLO'.encode())
+        infile = open(filename,'rb')
+        new_dict = pickle.load(infile)
+        print(new_dict)
+        infile.close()
+
+
+with socketserver.TCPServer(("", PORT), myHandler) as httpd:
     print("serving at port", PORT)
     httpd.serve_forever()
