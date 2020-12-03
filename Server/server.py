f12a01f0eb02b7a3078a6bc7f8a5fd5517ea4940
Lawand97 <lawand.97@hotmail.com>
2020-12-03 22:04:00 +0100

    get time from server

--- Server/server.py
+++ Server/server.py
@@ -2,6 +2,7 @@ from http.server import *
 import socketserver
 import pickle
 from scapy.all import *
+import time
 PORT = 3005
 #Handler = http.server.SimpleHTTPRequestHandler
 
@@ -10,26 +11,39 @@ def pi(filename,packets):
     outfile = open(filename,'wb')
     pickle.dump(packets,outfile)
     outfile.close()
-    
+
+
+def ping():
+    current_time = time.time()
+    return str(current_time)
+
+
 class myHandler(BaseHTTPRequestHandler):
     
     def do_GET(self):
-        ip =  self.path[8:]
-        print(ip)
-        packets =  sniff(timeout=5,filter =f'dst host {ip}')
-        filename = 'sniff'
-        print(self.path)
-        filename = filename + ip
-        pi(filename,packets)
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        #self.send_header("Content-length", len(b))
-        self.end_headers()
-        self.wfile.write('HELLO'.encode())
-        infile = open(filename,'rb')
-        new_dict = pickle.load(infile)
-        print(new_dict)
-        infile.close()
+        if self.path == 'ping'
+            result = ping()
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.send_header("Content-length", len(results))
+            self.end_headers()
+            self.wfile.write(f'{result}'.encode())  
+        # ip =  self.path[8:]
+        # print(ip)
+        # packets =  sniff(timeout=5,filter =f'dst host {ip}')
+        # filename = 'sniff'
+        # print(self.path)
+        # filename = filename + ip
+        # pi(filename,packets)
+        # self.send_response(200)
+        # self.send_header("Content-type", "text/html")
+        # #self.send_header("Content-length", len(b))
+        # self.end_headers()
+        # self.wfile.write('HELLO'.encode())
+        # infile = open(filename,'rb')
+        # new_dict = pickle.load(infile)
+        # print(new_dict)
+        # infile.close()
 
 
 with socketserver.TCPServer(("", PORT), myHandler) as httpd:
