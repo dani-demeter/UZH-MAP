89d5b60fcb3b1d382ee97c910d1daf152a3f44c2
Dani <flamegiraffes@gmail.com>
2020-12-06 17:03:07 -0500

    Added filtering in smart contract

--- Client/SmartContractTest.py
+++ Client/SmartContractTest.py
@@ -3,14 +3,13 @@ import env
 
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 
-
 myContract = w3.eth.contract(
     address=env.contractAddress, abi=env.contractABI)
 
 print(myContract.functions.get_output().call())
 
-# myContract.functions.addUserInfo(2000, 1313).transact(
+# myContract.functions.addMeasurement(2013, 1369, "ATT", [], 1001, 11, 1001, 16, 1001, 14).transact(
 #     {'from': w3.eth.accounts[0], "gas": 4000000})
 
-back = myContract.functions.getUserInfos().call()
-print(back)
+# back = myContract.functions.getAllMeasurements_ISP("Comcast").call()
+# print(back)
--- Smart Contract/Contract.sol
+++ Smart Contract/Contract.sol
@@ -8,24 +8,45 @@ contract TestContract
     struct UserInfo {
         int64 dist2server;
         int256 ping;
+        string ISP;
+        string[] hops;
+        int256 avgLatency_html; 
+        int256 avgJitter_html;
+        int256 avgLatency_video;
+        int256 avgJitter_video;
+        int256 avgLatency_audio;
+        int256 avgJitter_audio;
     }
+    
     
     UserInfo[] userInfos;
     
-    function addUserInfo(int64 d2s, int256 png) public returns(UserInfo[]){
-        userInfos.push(UserInfo({dist2server:d2s, ping:png}));
+    function addMeasurement(int64 d2s, int256 png, string isp, string[] hops2server, int256 al_html, int256 aj_html, int256 al_video, int256 aj_video, int256 al_audio, int256 aj_audio) public returns(uint256){
+        userInfos.push(UserInfo({dist2server:d2s, ping:png, ISP: isp, hops: hops2server, avgLatency_html: al_html, avgJitter_html: aj_html, avgLatency_video: al_video, avgJitter_video: aj_video, avgLatency_audio: al_audio, avgJitter_audio: aj_audio}));
+        return userInfos.length;
+    }
+    
+    function getAllMeasurements() public view returns(UserInfo[]){
         return userInfos;
     }
     
-    function getUserInfos() public view returns(UserInfo[]){
-        return userInfos;
+    function  (string filterISP) public view returns (UserInfo[]){
+        UserInfo[] memory res = new UserInfo[](userInfos.length);
+        bytes32 kFilterISP = keccak256(abi.encodePacked(filterISP));
+        uint256 counter = 0;
+        for (uint i=0; i<userInfos.length; i++) {
+          if(keccak256(abi.encodePacked(userInfos[i].ISP)) == kFilterISP){
+              res[counter] = userInfos[i];
+              counter += 1;
+          }
+        }
+        return res;
     }
     
     
-    // Defining a function 
     function get_output() public pure returns (string)  
     { 
        return ("Hi, your contract ran successfully!"); 
     } 
-  
+    
 } 
