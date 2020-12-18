c72315d9b5b4b7f473d939e31b43608f61861a27
Dani <flamegiraffes@gmail.com>
2020-12-18 14:26:57 -0500

    Bounties added to smart contract

--- Client/SmartContractTest.py
+++ Client/SmartContractTest.py
@@ -6,10 +6,22 @@ w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
 myContract = w3.eth.contract(
     address=env.contractAddress, abi=env.contractABI)
 
-print(myContract.functions.get_output().call())
+# print(myContract.functions.get_output().call())
 
-# myContract.functions.addMeasurement(2013, 1369, "ATT", [], 1001, 11, 1001, 16, 1001, 14).transact(
+# myContract.functions.addNewTestDescriptors(1000, 500, "ATT", []).transact(
 #     {'from': w3.eth.accounts[0], "gas": 4000000})
 
-# back = myContract.functions.getAllMeasurements_ISP("Comcast").call()
+# myContract.functions.addNewMeasurement("HTML", "jitter", 100)
+
+# back = myContract.functions.filterMeasurementsByISP("ATT").call()
 # print(back)
+
+# myContract.functions.placeBounty(2, 3, "ISP", ("ATT").encode('utf-8')).transact(
+#     {'from': w3.eth.accounts[0], "value": 6})
+
+back = myContract.functions.getQualifiedBounties(
+    1000, 500, ("ATT").encode('utf-8'), []).call()
+# print(back)
+
+res = myContract.functions.getBounty(back[0][0], back[1][0]).call()
+print(res)
--- Smart Contract/Contract.sol
+++ Smart Contract/Contract.sol
@@ -5,48 +5,112 @@ pragma experimental ABIEncoderV2;
 // Creating a contract 
 contract TestContract   
 { 
-    struct UserInfo {
+    struct MeasurementResult {
         int64 dist2server;
         int256 ping;
         string ISP;
         string[] hops;
-        int256 avgLatency_html; 
-        int256 avgJitter_html;
-        int256 avgLatency_video;
-        int256 avgJitter_video;
-        int256 avgLatency_audio;
-        int256 avgJitter_audio;
+        mapping(string => mapping(string => int256)) collectedMetrics;
+    }
+    
+    struct Bounty {
+        uint16 repeats;
+        uint256 bountyValue;
+        string bountyType;
+        bytes32 bountyReq;
     }
     
     
-    UserInfo[] userInfos;
+    mapping(address => MeasurementResult[]) measurementResults;
+    address[] addressesWithMeasurements;
+    uint256 numberOfMeasurements = 0;
     
-    function addMeasurement(int64 d2s, int256 png, string isp, string[] hops2server, int256 al_html, int256 aj_html, int256 al_video, int256 aj_video, int256 al_audio, int256 aj_audio) public returns(uint256){
-        userInfos.push(UserInfo({dist2server:d2s, ping:png, ISP: isp, hops: hops2server, avgLatency_html: al_html, avgJitter_html: aj_html, avgLatency_video: al_video, avgJitter_video: aj_video, avgLatency_audio: al_audio, avgJitter_audio: aj_audio}));
-        return userInfos.length;
+    mapping(address => mapping(uint => Bounty)) bounties;
+    mapping(address => bool) addressHasBounties;
+    address[] addressesWithBounties;
+    mapping(address => uint[]) bountyAddressTimestamps;
+    uint256 numberOfBounties = 0;
+    
+    function addNewTestDescriptors(int64 d2s, int256 png, string isp, string[] hops2server) public returns(uint256){
+        if(measurementResults[msg.sender].length == 0){
+            addressesWithMeasurements.push(msg.sender);
+        }
+        measurementResults[msg.sender].push(MeasurementResult({dist2server:d2s, ping:png, ISP: isp, hops: hops2server}));
+        numberOfMeasurements++;
+        return measurementResults[msg.sender].length;
     }
     
-    function getAllMeasurements() public view returns(UserInfo[]){
-        return userInfos;
+    function addNewMeasurement(string mediaType, string metricType, int256 measurementValue) public returns(uint256){
+        require(measurementResults[msg.sender].length > 0);
+        measurementResults[msg.sender][measurementResults[msg.sender].length - 1].collectedMetrics[mediaType][metricType] = measurementValue;
+        return 0;
     }
     
-    function  (string filterISP) public view returns (UserInfo[]){
-        UserInfo[] memory res = new UserInfo[](userInfos.length);
+    function filterMeasurementsByISP(string filterISP) public view returns (address[], uint256[]){
+        address[] memory resAddressesFull = new address[](numberOfMeasurements);
+        uint256[] memory resIndexesFull = new uint256[](numberOfMeasurements);
         bytes32 kFilterISP = keccak256(abi.encodePacked(filterISP));
         uint256 counter = 0;
-        for (uint i=0; i<userInfos.length; i++) {
-          if(keccak256(abi.encodePacked(userInfos[i].ISP)) == kFilterISP){
-              res[counter] = userInfos[i];
-              counter += 1;
-          }
+        for (uint i=0; i<addressesWithMeasurements.length; i++) {
+            for(uint j = 0; j<measurementResults[addressesWithMeasurements[i]].length; j++){
+                if(keccak256(abi.encodePacked(measurementResults[addressesWithMeasurements[i]][j].ISP)) == kFilterISP){
+                    resAddressesFull[counter] = addressesWithMeasurements[i];
+                    resIndexesFull[counter] = j;
+                    counter += 1;
+                }
+            }
         }
-        return res;
+        address[] memory resAddresses = new address[](counter);
+        uint256[] memory resIndexes = new uint256[](counter);
+        for(uint k = 0; k<counter; k++){
+            resAddresses[k] = resAddressesFull[k];
+            resIndexes[k] = resIndexesFull[k];
+        }
+        return (resAddresses, resIndexes);
     }
     
     
-    function get_output() public pure returns (string)  
-    { 
-       return ("Hi, your contract ran successfully!"); 
-    } 
+    function placeBounty(uint256 valuePerBounty, uint16 numRepeats, string bType, bytes32 bReq) external payable{
+        require(msg.value==valuePerBounty*numRepeats);
+        if(!addressHasBounties[msg.sender] || bounties[msg.sender][block.timestamp].repeats==0){
+            numberOfBounties += 1;
+            addressHasBounties[msg.sender] = true;
+            addressesWithBounties.push(msg.sender);
+            bountyAddressTimestamps[msg.sender].push(block.timestamp);
+        }
+        bounties[msg.sender][block.timestamp] = Bounty({repeats:numRepeats, bountyType:bType, bountyReq:bReq, bountyValue:msg.value});
+    }
+    
+    function getQualifiedBounties(int64 d2s, int256 png, bytes32 isp, string[] hops2server) public view returns(address[], uint256[]){
+        address[] memory resAddressesFull = new address[](numberOfBounties);
+        uint256[] memory resIndexesFull = new uint256[](numberOfBounties);
+        
+        // bytes32 kFilterISP = keccak256(abi.encodePacked(isp));
+        uint256 counter = 0;
+        
+        for (uint i=0; i<addressesWithBounties.length; i++) {
+            for(uint j = 0; j<bountyAddressTimestamps[addressesWithBounties[i]].length; j++){
+                bytes32 typeOfBounty = keccak256(abi.encodePacked(bounties[addressesWithBounties[i]][bountyAddressTimestamps[addressesWithBounties[i]][j]].bountyType));
+                bytes32 bountyReq = keccak256(abi.encodePacked(bounties[addressesWithBounties[i]][bountyAddressTimestamps[addressesWithBounties[i]][j]].bountyReq));
+                if(typeOfBounty == keccak256(abi.encodePacked("ISP")) && bountyReq == keccak256(abi.encodePacked(isp))){
+                    resAddressesFull[counter] = addressesWithMeasurements[i];
+                    resIndexesFull[counter] = bountyAddressTimestamps[addressesWithBounties[i]][j];
+                    counter += 1;
+                }
+            }
+        }
+        address[] memory resAddresses = new address[](counter);
+        uint256[] memory resIndexes = new uint256[](counter);
+        for(uint k = 0; k<counter; k++){
+            resAddresses[k] = resAddressesFull[k];
+            resIndexes[k] = resIndexesFull[k];
+        }
+        return (resAddresses, resIndexes);
+    }
+    
+    function getBounty(address addr, uint timest) public view returns(Bounty){
+        require(addressHasBounties[addr] && bounties[addr][timest].repeats>0);
+        return bounties[addr][timest];
+    }
     
 } 
